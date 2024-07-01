d14293fea71e7278dfd44a8e8197bfc0785baf25
Aleksandr Sizov <als.sizov@gmail.com>
2024-07-01 15:12:40 +0400

    Add IO operations

--- src/graph.rs
+++ src/graph.rs
@@ -1,7 +1,7 @@
 pub trait Graph<N> {
     type NodeIterator: Iterator<Item=&N>;
     type NeighbourIterator: Iterator<Item=&N>;
-    type EdgeIterator: Iterator<Item=(&N, &N)>;
+    type EdgeIterator: Iterator<Item(&N, &N)>;
 
     fn is_empty(&self) -> bool;
     fn len(&self) -> usize;
