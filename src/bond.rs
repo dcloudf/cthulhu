1904bf61635f13031d469039be33c10112366c6c
dcloudf <139797069+dcloudf@users.noreply.github.com>
2024-07-02 10:46:05 +0400

    Establish CI pipeline (#2)
    * Add 2 stages to the pipeline
    
    * Change the testing yaml
    
    * Remove redundant file
    
    * Change the name
    
    * Add allowing of dead code
    
    * Add Miri flag for isolation

--- src/bond.rs
+++ src/bond.rs
@@ -1,9 +1,11 @@
+#[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 pub(crate) struct Bond {
     pub(crate) indices: (usize, usize),
     pub(crate) order: BondOrder,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 pub enum BondOrder {
     Single,
